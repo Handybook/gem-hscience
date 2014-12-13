2c5b04d38d261a8338cc24c9621de674315930c2
Jose Rivera <jrivera-handy@handybook.com>
2014-12-12 22:48:45 -0500

    initial commit

---
+++ lib/hyper_science.rb
@@ -0,0 +1,45 @@
+require 'json'
+require 'net/http'
+
+module HyperScience
+  module ClassMethods
+    attr_accessor :host, :port, :enabled
+  end
+
+  class << self
+    include ClassMethods
+
+    def get_model(datasource, model)
+      Model.new(self, datasource, model)
+    end
+  end
+
+  class Model
+    #include ClassMethods
+    attr_accessor :datasource, :model, :http_client, :enabled
+
+    def initialize(source, datasource, model)
+      @datasource = datasource
+      @model = model
+      @http_client = Net::HTTP.new(source.host, source.port)
+      @enabled = source.enabled
+    end
+
+    def calculate(id)
+      return unless @enabled
+      request = Net::HTTP::Post.new("/api/v2/predict/#{@datasource}?columns=#{@model}")
+      request.add_field "Content-Type", "application/json"
+      request.body = "{\"params\":{\"id\":\"#{id}\"}}"
+      response = http_client.request(request)
+      JSON.parse( response.body )["result"][0]
+    end
+
+    def refit
+      return unless @enabled
+      request = Net::HTTP::Post.new("/api/v2/analyze/#{@datasource}?columns=#{@model}")
+      request.add_field "Content-Type", "application/json"
+      response = http_client.request(request)
+      JSON.parse( response.body )["job"]
+    end
+  end
+end
---
+++ lib/tasks/hyper_science_tasks.rake
@@ -0,0 +1,4 @@
+# desc "Explaining what the task does"
+# task :hyper_science do
+#   # Task goes here
+# end
